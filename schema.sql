c1e3891ffed0fc4d5a77e0977bb454054a6d88c0
Michael Munavu <86654131+MICHAELMUNAVU83@users.noreply.github.com>
2022-08-17 07:11:30 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -9,3 +9,15 @@ ALTER TABLE animals  ALTER COLUMN id  SET NOT NULL;
 
 ALTER TABLE animals
 ADD COLUMN species varchar(200);
+CREATE TABLE owners ( id  serial primary key ,full_name varchar(200), age INTEGER);
+CREATE TABLE species ( id  serial primary key ,name varchar(200));
+ALTER TABLE animals DROP COLUMN id;
+ALTER TABLE animals ADD id serial primary key;
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD species_id INTEGER;
+ADD CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species(id);
+ALTER TABLE animals ADD owner_id INTEGER;
+ALTER TABLE animals
+ADD CONSTRAINT fk_owner FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+
